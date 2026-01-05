397057478e91760f68b47483f894e9971ca26fbd
likitha <konamlikitha@gmail.com>
2026-01-05 22:17:05 +0530

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Payment payment = new Payment("PAY123", 500.0);
-
-        PaymentProcessor processor = new UpiPaymentProcessor();
-
         PaymentService service = new PaymentService();
+        PaymentProcessor upiProcessor = new UpiPaymentProcessor();
 
-        boolean result = service.processPayment(payment, processor);
+        Payment payment1 = new Payment("P1", 200, "K1");
+        Payment payment2 = new Payment("P2", 400, "K2");
+        Payment payment3 = new Payment("P3", 600, "K3");
 
-        payment.printDetails();
-        System.out.println("Payment success: " + result);
+        service.processPayment(payment1, upiProcessor);
+        service.processPayment(payment2, upiProcessor);
+        service.processPayment(payment3, upiProcessor);
+
+        System.out.println("\nALL PAYMENTS:");
+        for (Payment p : service.getAllPayments()) {
+            p.printDetails();
+        }
+
+        System.out.println("\nSUCCESS PAYMENTS:");
+        for (Payment p : service.getPaymentsByStatus(PaymentStatus.SUCCESS)) {
+            p.printDetails();
+        }
     }
 }
